577f3d1e7ccbefe8612ddb2a80b000a1e84a0b1e
Diego Escobar <89594898+diegoscbr@users.noreply.github.com>
2024-02-26 00:17:44 +0000

    cd now viewable in command history

--- gush.c
+++ gush.c
@@ -77,6 +77,7 @@ int main(int argc, char *argv[]) {
             {
                 perror("chdir");
             }
+            insertAtEnd(&commandHistory, command);
             printf("gush> ");
             continue;
         }
